e893742f0d8665d04f653fd0594c7bf0ec3edf03
Brendan McGinty <brendan.mcginty14@gmail.com>
2024-01-29 18:05:04 -0500

    added the first batch of questions objects for user prompt, and boilerplate function to write to file

--- Assets/index.js
+++ Assets/index.js
@@ -1,13 +1,53 @@
 // TODO: Include packages needed for this application
+const path = require('path');
+const fs = require('fs');
+const inquirer = require('inquirer');
+const markdown = require('./generateMarkdown');
+const app = express();
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your Github username?'
+    },
+    { 
+        type: "input",
+        name: "email",
+        message: 'What is email?',
+     },
+    { 
+       type: "input",
+       name: "title",
+       message: 'What is the title of your project?',
+    },
+    { 
+       type: 'input',
+       name: "description",
+       message: 'What is a description of your project?'
+    },
+    { 
+        type: 'input',
+        name: 'instructions',
+        message: 'What are the instructions to use the project?'
+    },
+    { 
+        type: 'list',
+        name: 'license',
+        message: 'What license (if any) does your project use?',
+        choices: ['MIT', 'APACHE','GPL', 'None']
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFileSync(path.join(process.cwd(), fileName), data)
+ }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
